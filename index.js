5a7b75ce64db6de54ed5be1330bbff8a87e05e12
cjcautivar <chris.cautivar@gmail.com>
2017-09-21 19:49:00 -0700

    ore

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+//serverside javascript
+//for concrete explinations of express functions see the frameowr (aka library)
 var express = require('express');
-var app = express();
 //including the express library, but in javascript
+var app = express();
+//app is an "instance" of our express server
 app.use(express.static(__dirname));
 //acessing express in its directory name (thx express for the global variable)
+app.get('/hello',function(req,res){
+  res.json({data:'hello world'});
+})
+//req is query
+//res is response
 app.listen(3000);
-//port 3000's open
+//send this file diretory to port 3000
